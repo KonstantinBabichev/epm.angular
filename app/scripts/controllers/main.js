a3956939fb6f5c1033d4ed36f1e9b45aa52e72a4
KonstantinBabichev <kanstantsin_babichau@epam.com>
2014-08-03 23:52:52 +0300

    forgotten date feature added

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,35 +8,46 @@
  * Controller of the fuzzyApp
  */
 angular.module('fuzzyApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $filter) {
     $scope.articles = [
       {
         author: 'Onliner.by',
         title: 'Bose подала в суд на Beats из-за технологии шумоподавления в наушниках',
         introText: 'Компания Bose обвинила Beats в неправомерном использовании технологии шумоподавления в моделях наушников Beats Studio и Beats Studio Wireless. В поданных в суд документах юристы Bose указывают на длительную историю разработок и инвестиций в исследования технологии шумоподавления и требуют применить санкции в отношении конкурента, если нарушение патента было умышленным.',
-        image: 'images/article-1.jpg'
+        image: 'images/article-1.jpg',
+        createDate: '1404421200000',
+        date: '04.07.2014'
       },
       {
         author: 'Onliner.by',
         title: 'Кинокритики советуют не тратить время на блокбастер «Геракл»',
         introText: 'Стартовал мировой и белорусский прокат очередного летнего блокбастера. Кинокритики успели высказать свое мнение о «Геракле», причем в целом ленту они приняли негативно. Средняя оценка на Metacritic составляет всего 48 из 100 (на основе 20 рецензий), а на Rotten Tomatoes — 5,4 из 10 (72 отзыва).',
-        image: 'images/article-2.jpg'
+        image: 'images/article-2.jpg',
+        createDate: '1404334800000',
+        date: '03.07.2014'
       },
       {
         author: 'Onliner.by',
         title: 'Австралийская полиция арестовала «Дэдпула» с мечами и кексами',
         introText: 'Австралийский косплеер в образе Дэдпула не смог вовремя добраться до места проведения благотворительного мероприятия, так как полиция посчитала всю бутафорию на нем настоящим оружием. По данным издания The Daily Telegraph, мужчина привлек внимание горожан своим костюмом, высоким ростом, а также гранатой, самурайским мечом, лентами патронов и коробкой с кексами.',
-        image: 'images/article-3.jpg'
+        image: 'images/article-3.jpg',
+        createDate: '1404248400000',
+        date: '02.07.2014'
       },
       {
         author: 'Onliner.by',
         title: 'Nokia предложила заряжать смартфон Lumia 930 с помощью картошки',
         introText: 'Британское подразделение Nokia совместно с художником и ученым-энтузиастом Калебом Чарлэндом, а также сетью розничных магазинов мобильной электроники Carphone Warehouse организовало проект, в рамках которого был представлен «самый органический способ зарядки». Для получения энергии использовались 800 объединенных между собой картофелин и яблок. ',
-        image: 'images/article-4.jpg'
+        image: 'images/article-4.jpg',
+        createDate: '1404162000000',
+        date: '01.07.2014'
       }
     ];
 
     $scope.addArticle = function () {
+      var dateNow = new Date();
+      $scope.newArticle.createDate = dateNow;
+      $scope.newArticle.date = $filter('date')(dateNow, 'dd.MM.yyyy');
       $scope.articles.unshift($scope.newArticle);
       $scope.resetForm();
       $scope.togglePopUp();
@@ -50,9 +61,4 @@ angular.module('fuzzyApp')
     $scope.togglePopUp = function () {
       $scope.showForm = !$scope.showForm;
     };
-  })
-
-//  .controller('formController', function ($scope) {
-//
-//  })
-;
+  });
