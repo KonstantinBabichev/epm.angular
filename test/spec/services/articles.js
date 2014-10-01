1c9d084e20a09dcfc6caabdff75b15b4ce153c68
KonstantinBabichev <kanstantsin_babichau@epam.com>
2014-10-01 22:31:35 +0300

    HM 7 - services covered

--- test/spec/services/articles.js
+++ test/spec/services/articles.js
@@ -1,14 +1,46 @@
 'use strict';
 
-describe('Service: articles', function () {
+describe('Service: Articles', function () {
+  var $httpBackend,
+    articles,
+    queryRespond = [
+      {
+        _id: '540d73dafef38b74188ecde4',
+        author: 'Author 1',
+        body: 'Lorem Ipsum',
+        date: '2014/10/01',
+        title: 'Title 1'
+      },
+      {
+        _id: '540d73dafef38b74188ecde5',
+        author: 'Author 2',
+        body: 'Lorem Ipsum',
+        date: '2014/10/01',
+        title: 'Title 2'
+      },
+      {
+        _id: '540d73dafef38b74188ecde6',
+        author: 'Author 3',
+        body: 'Lorem Ipsum',
+        date: '2014/10/01',
+        title: 'Title 3'
+      }];
 
-  // load the service's module
   beforeEach(module('fuzzyApp'));
 
-  // instantiate service
-  var articles;
-  beforeEach(inject(function (_articles_) {
-    articles = _articles_;
+  beforeEach(inject(function ($injector) {
+    $httpBackend = $injector.get('$httpBackend');
+    $httpBackend.when('GET', 'http://54.72.3.96:3000/posts').respond(queryRespond);
+
+    articles = $injector.get('Articles');
   }));
 
+  it('should slice text', function () {
+    var articlesList  = articles.query();
+    expect(articlesList.length).toBe(0);
+
+    $httpBackend.flush();
+
+    expect(articlesList.length).toBeGreaterThan(0);
+  });
 });
